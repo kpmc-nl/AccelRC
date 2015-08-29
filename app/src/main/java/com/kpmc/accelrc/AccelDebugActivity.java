7a1087cb5499d224c7f14fc6165a2b9d98645467
Matthijs Oosterhoff <matthijs.oosterhoff@gmail.com>
2015-08-29 10:07:39 +0200

    Initial version with simple Accel Debug activity

---
+++ app/src/main/java/com/kpmc/accelrc/AccelDebugActivity.java
@@ -0,0 +1,138 @@
+package com.kpmc.accelrc;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.hardware.Sensor.TYPE_ACCELEROMETER;
+
+public class AccelDebugActivity extends Activity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelSensor;
+
+    private ProgressBar azimuthBar, pitchBar, rollBar;
+    private TextView maxAzimuthTV, maxPitchTV, maxRollTV;
+
+    private float maxAzimuth, maxPitch, maxRoll;
+
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        azimuthBar = (ProgressBar) findViewById(R.id.azimuthBar);
+        pitchBar = (ProgressBar) findViewById(R.id.pitchBar);
+        rollBar = (ProgressBar) findViewById(R.id.rollBar);
+
+        maxAzimuthTV = (TextView) findViewById(R.id.maxAzimuth);
+        maxPitchTV = (TextView) findViewById(R.id.maxPitch);
+        maxRollTV = (TextView) findViewById(R.id.maxRoll);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER);
+
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (null == bluetoothAdapter) {
+            Toast.makeText(this, "No bluetooth adapter.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, "Bluetooth not enabled.", Toast.LENGTH_SHORT).show();
+        }
+
+        for (BluetoothDevice dev : bluetoothAdapter.getBondedDevices()) {
+            if("HC-06".equals(dev.getName())){
+                Toast.makeText(this, "Found the right device!", Toast.LENGTH_SHORT).show();;
+                new BTTestThread(dev, this).start();
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (null == accelSensor) {
+            Log.e("AccelDebugActivity", "Got null for sensor type TYPE_ACCELEROMETER");
+            Toast.makeText(this, "NULL FOR ACCELEROMETER", Toast.LENGTH_SHORT).show();
+        } else {
+            // For now, take the default sampling rate for games. Should be sufficient for RC use.
+            sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+      /*
+       * According to the Android documentation:
+       *
+       * event.values[0]: azimuth, rotation around the Z axis.
+       * event.values[1]: pitch, rotation around the X axis.
+       * event.values[2]: roll, rotation around the Y axis.
+       */
+
+        float valueAzimuth = event.values[0];
+        float valuePitch = event.values[1];
+        float valueRoll = event.values[2];
+
+        maxAzimuth = Math.max(Math.abs(valueAzimuth), maxAzimuth);
+        maxPitch = Math.max(Math.abs(valuePitch), maxPitch);
+        maxRoll = Math.max(Math.abs(valueAzimuth), maxRoll);
+
+        maxAzimuthTV.setText(valueAzimuth + "/" + maxAzimuth);
+        maxPitchTV.setText(valuePitch + "/" + maxPitch);
+        maxRollTV.setText(valueRoll + "/" + maxRoll);
+
+        azimuthBar.setProgress(50 + (int) (50 * valueAzimuth / maxAzimuth));
+        pitchBar.setProgress(50 + (int) (50 * valuePitch / maxPitch));
+        rollBar.setProgress(50 + (int) (50 * valueRoll / maxRoll));
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d("AccelDebugActivity", "Accuracy changed: " + sensor.getName());
+    }
+}
---
+++ app/src/main/java/com/kpmc/accelrc/BTTestThread.java
@@ -0,0 +1,81 @@
+package com.kpmc.accelrc;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+/**
+ * Created by matthijs on 26/08/15.
+ */
+public class BTTestThread extends Thread {
+
+    private final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private BluetoothSocket btSocket;
+    private Activity context;
+
+    public BTTestThread(BluetoothDevice device, Activity context) {
+        this.context = context;
+        try {
+            btSocket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void run() {
+
+        int counter = 0;
+
+        try {
+            while (true) {
+                ++counter;
+                final String s = "count " + counter + "\n";
+                context.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                btSocket.getOutputStream().write(s.getBytes("UTF-8"));
+
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+
+                if (Thread.currentThread().interrupted()) {
+                    return;
+                }
+            }
+        } catch (IOException e) {
+            Log.e("BTTestThread", "IOException: " + e.getMessage());
+        } finally {
+            cancel();
+        }
+    }
+
+    /**
+     * Will cancel the listening socket, and cause the thread to finish
+     */
+    public void cancel() {
+        if (null != btSocket) {
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+}
+
+
